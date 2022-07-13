7ac1c2d513e69935f3fe9bf12c60dea71864fd0e
Brian <bkimutai2021@gmail.com>
2022-07-13 14:37:28 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Links from "../components/Links"
+import user from "../data/user"
 
-function About() {
+
+function About(props) {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       <h2>About Me</h2>
+       
+       
+       {props.bio!=null ? (
+        <p>{props.bio}</p>
+      ) : (
+      ''
+      )}
+       
+       
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links links={user.links}/>
+
     </div>
   );
 }
